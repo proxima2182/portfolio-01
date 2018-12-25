761854ddb4adf3b4935e7aa52ba898700470f362
Terry An <arcrise@naver.com>
2018-12-25 17:55:27 +0900

    Update common.js

--- js/common.js
+++ js/common.js
@@ -205,7 +205,7 @@ function resize_standard() {
             }
             if(IS_IOS) {
 //                setTimeout(function(){
-                    input_wrap.find("input").focus();
+                    input_wrap.find("input").data("focused").focus();
 //                },250);
             }
         }
